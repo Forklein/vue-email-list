b743b1c14e0432eb8e647069b8473d2de1b62121
Forklein <giuseppepisani2@gmail.com>
2021-09-01 16:40:28 +0200

    add v-for and empty array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 console.log('Vue is activated', Vue);
 Vue.config.devtools = true;
+
+const root = new Vue({
+    el: '#app',
+    data: {
+        test: 'Ciao Vue',
+        emails: [],
+    },
+    methods: {
+        getEmail(arr) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    const currentEmail = res.data.response;
+                    arr.push(currentEmail);
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        },
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            this.getEmail(this.emails)
+        }
+    }
+})
